3262ce4cba5bd928193aae4bfce4444aa50e2cb5
aileen-reichelt <61852917+aileen-reichelt@users.noreply.github.com>
2020-12-18 17:49:24 +0100

    added documentation, corrected typo and returns
    - added docstring for the class
    - changed "selt.most_frequent_words" to "self.most_frequent_words"
    - changed return relations/return most_frequent_words to return self.relations/self.most_frequent_words

--- data/character.py
+++ data/character.py
@@ -1,18 +1,19 @@
 import Relation
 
 class Character:
+"""Simple class to store and retrieve information about a character. Specifially: name, aliases for the name, relations to other characters, keywords."""
 
     def __init__(self, name):
         self.name = name
         self.alias = list()
         self.relations = list()
-        selt.most_frequent_words = list()
+        self.most_frequent_words = list()
     
     def add_relation(self, target_character):
         self.relations.append(Relation(self, target_character))
 
     def get_relations(self):
-        return relations
+        return self.relations
     
     def get_most_frequent_words(self):
-        return most_frequent_words
+        return self.most_frequent_words
