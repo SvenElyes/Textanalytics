f28c58387e27c11b2bb5c9dc35836ea4807e6d49
JohannesKlueh <joe.klueh@gmx.de>
2020-12-23 19:11:16 +0100

    function for adding character column to df

--- src/named_entities.py
+++ src/named_entities.py
@@ -9,14 +9,24 @@ df_bible = dataloader.get_df_bible()
 
 def get_characters_from_text(text):
     doc = nlp(text)
+    characters = set()
     for entity in doc.ents:
         if entity.label_ == "PERSON":
-            print(entity.label_, ' | ', entity.text)
+            #print(entity.label_, ' | ', entity.text)
+            characters.add(entity.text)
+    return characters
 
 def show_word_mapping(text):
     doc = nlp(text)
     displacy.serve(doc, style='dep')
 
+def add_character_column(df):
+    df.insert(len(df.columns), "characters", None, True)
+    for i in range(len(df)):
+        characters = get_characters_from_text(df["text"][i])
+        df["characters"][i] = "| ".join(characters)
+
+test_df = df_bible.head(100)
+
 if __name__ == "__main__":
-    for verse in df_bible["text"]:
-        get_characters_from_text(verse)
+    add_character_column(test_df)
