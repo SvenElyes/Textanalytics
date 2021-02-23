e93a337c3751e7ab3e7bf70ec0f097a47c2f2c7e
Felix Munzlinger <66962505+fmunzlin@users.noreply.github.com>
2021-02-23 18:25:56 +0100

    centralized main, re-ordered calls

--- main.py
+++ main.py
@@ -11,16 +11,37 @@ from os.path import exists
 import src.dataloader as dataloader
 import src.pickle_handler as pickle_handler
 import src.relation_creator as relation_creator
-import preprocess_emotion
-import join_df
-import eval_graph
+import src.preprocess_emotion as preprocess_emotion
+import src.join_df as join_df
+import src.eval_graph as eval_graph
+import pandas as pd
+import os
+
+
+def main(testament="both"):
+    os.makedirs("src/csv", exist_ok=True)
+    df_bible = None
+
+    # check if a dataframe is given or start setting up on by preprocess_emotion.main()
+    if exists("src/csv/bibleTA_prepro.csv") == False:
+        df_bible = preprocess_emotion.main("both")
+    else:
+        df_bible = pd.read_csv("src/csv/bibleTA_prepro.csv")
+
+    # unite two dataframes since the calculation of "bibleTA_prepro.csv" takes a substancial amount of time
+    df_bible = join_df.main(character_csv="src/csv/bibleTA_characters.csv", relation_csv="src/csv/bibleTA_prepro.csv",
+                            out_csv="src/csv/bibleTA_emotion.csv", df_bible=df_bible)
+
+    # if a specific testament is given, reduce dataframe
+    if testament == "new":
+        _, df_bible = dataloader.get_old_new_testament(df_bible)
+    if testament == "old":
+        df_bible, _ = dataloader.get_old_new_testament(df_bible)
+
+    # determine the graph, create pickle objects and run a clustering of keywords
+    eval_graph.main(threshold_getgraph=5, num_cluster=4, threshold_getcluster=(1 / 6),
+                    file="src/csv/bibleTA_emotion.csv", load=True, df_bible=df_bible)
 
 if __name__ == "__main__":
-    if exists("bibleTA_prepro.csv") == False:
-        df_bible = preprocess_emotion.main("both")
+    main()
 
-    join_df.main(character_csv="bibleTA_characters.csv", relation_csv="bibleTA_prepro.csv", out_csv="bibleTA_emotion.csv")
-    eval_graph.main(threshold_getgraph=5, num_cluster=4, threshold_getcluster=(1 / 6),
-                    file="bibleTA_emotion.csv", load=True)
-
- 
