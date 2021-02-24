c2c2ca023416a98296dc57b71301162548015645
Felix Munzlinger <66962505+fmunzlin@users.noreply.github.com>
2021-02-24 11:31:00 +0100

    minor changes

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import src.join_df as join_df
 import src.eval_graph as eval_graph
 import pandas as pd
 import os
-
+import numpy as np
 
 def main(testament="both"):
     os.makedirs("src/csv", exist_ok=True)
@@ -37,7 +37,14 @@ def main(testament="both"):
         _, df_bible = dataloader.get_old_new_testament(df_bible)
     if testament == "old":
         df_bible, _ = dataloader.get_old_new_testament(df_bible)
+    top = pd.DataFrame()
+    columns = df_bible.columns.tolist()[1:]
 
+    for idx in np.random.randint(0, df_bible.shape[0], 10):
+        top = top.append(df_bible.loc[idx])
+    print(top)
+
+    top.to_csv("test/test_eval_graph.csv")
     # determine the graph, create pickle objects and run a clustering of keywords
     eval_graph.main(threshold_getgraph=5, num_cluster=4, threshold_getcluster=(1 / 6),
                     file="src/csv/bibleTA_emotion.csv", load=True, df_bible=df_bible)
